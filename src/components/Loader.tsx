e2c904652780e7e2d3bcff510499d84a49da848c
shunkosa <shun.kosaka.102@gmail.com>
2021-01-21 19:29:30 +0900

    🔨 tsx

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,37 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { StyleSheet, View, Modal, ActivityIndicator, Text } from 'react-native';
 import { APP_THEME, APP_FONTS } from '../constants';
 
-const Loader = props => {
-  const { loading, message, ...attributes } = props;
+type LoaderProps = {
+  loading: boolean;
+  message?: string;
+};
+
+function Loader(props: LoaderProps) {
+  const { loading, message } = props;
   return (
     <Modal transparent visible={loading}>
       <View style={styles.modalBackground}>
-        <ActivityIndicator
-          animating={loading}
-          size="large"
-          hidesWhenStopped
-          color={APP_THEME.APP_BASE_COLOR}
-        />
+        <ActivityIndicator animating={loading} size="large" hidesWhenStopped color={APP_THEME.APP_BASE_COLOR} />
         <Text style={styles.loadingTitleStyle}> {message} </Text>
       </View>
     </Modal>
   );
-};
+}
+
 const styles = StyleSheet.create({
   modalBackground: {
     flex: 1,
     justifyContent: 'center',
     backgroundColor: '#fff',
     alignItems: 'center',
-    opacity: 0.8
+    opacity: 0.8,
   },
   loadingTitleStyle: {
     fontFamily: APP_FONTS.FONT_REGULAR,
     fontSize: 36,
     color: APP_THEME.APP_BASE_COLOR,
-    paddingTop: 10
-  }
+    paddingTop: 10,
+  },
 });
 
 export { Loader };
