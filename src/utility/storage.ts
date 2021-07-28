71e5658cf1604e5edad283a477f5eb359b4a95a8
shunkosa <shun.kosaka.102@gmail.com>
2021-07-29 00:19:29 +0900

    🐞 Fix import of async-storage

--- src/utility/storage.ts
+++ src/utility/storage.ts
@@ -1,4 +1,4 @@
-import AsyncStorage from '@react-native-community/async-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import Storage from 'react-native-storage';
 
 import { ASYNC_STORAGE_KEYS } from '../constants';
