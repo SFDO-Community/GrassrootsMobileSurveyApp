af8c320284515dcb39824cb79ab01347ccb3a444
Shun Kosaka <shun.kosaka.102@gmail.com>
2022-05-05 18:39:35 +0900

    🔧 Upgrade to expo 44 (#81)
    * Upgrade to expo 44 and fix jest tests
    * Add @react-native-picker/picker
    * 🐞 Fix typo in snapshot

--- jest.setup.js
+++ jest.setup.js
@@ -1,3 +1,6 @@
+import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
+import 'react-native-gesture-handler/jestSetup';
+
 const mockedNavigation = {
   navigate: jest.fn(),
   goBack: jest.fn(),
@@ -16,4 +19,16 @@ jest.mock('@react-navigation/native', () => {
   };
 });
 
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+jest.mock('react-native-reanimated', () => {
+  const Reanimated = require('react-native-reanimated/mock');
+
+  // The mock for `call` immediately calls the callback which is incorrect
+  // So we override it with a no-op
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  Reanimated.default.call = () => {};
+
+  return Reanimated;
+});
 jest.mock('react-native-elements/src/icons/Icon', () => 'View');
+jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
