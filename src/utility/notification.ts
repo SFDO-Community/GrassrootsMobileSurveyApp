62736fb2195ac46aae240dd26e828d3e13cf07f8
Shun Kosaka <shun.kosaka.102@gmail.com>
2022-05-06 10:05:51 +0900

    ✨ Update survey title with mode and update dependencies (#87)
    * ✨ Update survey title with mode
    * Update dependencies

--- src/utility/notification.ts
+++ src/utility/notification.ts
@@ -16,6 +16,7 @@ export const notifySuccess = (description: string) => {
     icon: {
       icon: 'success',
       position: 'left',
+      props: {},
     },
     backgroundColor: APP_THEME.APP_SUCCESS_COLOR,
     duration: 3000,
@@ -33,6 +34,7 @@ export const notifyError = (description: string) => {
     icon: {
       icon: 'danger',
       position: 'left',
+      props: {},
     },
     autoHide: false,
     backgroundColor: APP_THEME.APP_ERROR_COLOR,
