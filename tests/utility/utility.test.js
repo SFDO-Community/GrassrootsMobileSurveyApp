4c9f15019eddbc61d6650ee6853d99a17c86f03e
shunkosa <shun.kosaka.102@gmail.com>
2022-05-07 23:40:16 +0900

    🐞 Fix login email validation

--- tests/utility/utility.test.js
+++ tests/utility/utility.test.js
@@ -5,6 +5,9 @@ describe('Utility', () => {
     const email = 'test@example.com';
     expect(validateEmail(email)).toBeTruthy();
 
+    const emailWithPlusCharacter = 'test+example.123@gmail.com';
+    expect(validateEmail(emailWithPlusCharacter)).toBeTruthy();
+
     const notEmail = 'test';
     expect(validateEmail(notEmail)).toBeFalsy();
   });
