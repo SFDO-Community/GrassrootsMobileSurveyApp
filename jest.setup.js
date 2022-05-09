6538ec82e629f3b29ae52b44d3596247c7fefc39
shunkosa <shun.kosaka.102@gmail.com>
2022-05-09 15:44:55 +0900

    🗑 Remove unused dependencies

--- jest.setup.js
+++ jest.setup.js
@@ -19,16 +19,5 @@ jest.mock('@react-navigation/native', () => {
   };
 });
 
-jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
-jest.mock('react-native-reanimated', () => {
-  const Reanimated = require('react-native-reanimated/mock');
-
-  // The mock for `call` immediately calls the callback which is incorrect
-  // So we override it with a no-op
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  Reanimated.default.call = () => {};
-
-  return Reanimated;
-});
 jest.mock('react-native-elements/src/icons/Icon', () => 'View');
 jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
