05f5811eeefa8a14f8242d5a5242a5b2402edc91
shunkosa <shun.kosaka.102@gmail.com>
2022-05-17 19:01:04 +0900

    🔧 Add coveragePathIgnorePatterns

--- jest.config.js
+++ jest.config.js
@@ -10,4 +10,5 @@ module.exports = {
     'node_modules/(?!(jest-)?react-native|@react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)',
   ],
   testEnvironment: 'jsdom',
+  coveragePathIgnorePatterns: ['<rootDir>/assets/', '<rootDir>/tests/'],
 };
